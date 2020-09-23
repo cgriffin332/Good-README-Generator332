18da2eb6500a39345a3d52893990634f792406f2
cgriffin332 <cgriffin332@gmail.com>
2020-09-23 18:51:51 -0400

    installed inquirer

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,10 @@
+const fs = require("fs");
+const util = require("util");
+
+  // Promise-based versions of functions using node style callbacks
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
